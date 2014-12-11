9d62df17a7dba6c98a35858f52e4eb2172045dd5
quadnix <quadnix1@gmail.com>
2014-12-11 04:36:27 -0500

    Added a menu

---
+++ client/common/common.js
@@ -0,0 +1,9 @@
+Template.menu.helpers {
+  'role': function() {
+    ref = Roles.findOne({ref: Meteor.userId()});
+    if (ref != undefined) {
+      return ref.role;
+    }
+    return 'student';
+  }
+}
